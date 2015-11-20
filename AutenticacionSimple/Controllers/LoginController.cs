fba99c3385c554ad74c5bae198d18dfc65496922
FranLsz <franlsz95develop@gmail.com>
2015-11-20 14:24:41 +0100

    first commit

---
+++ AutenticacionSimple/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using AutenticacionSimple.Models;
+
+namespace AutenticacionSimple.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Index(Usuario model)
+        {
+            if (model.Login.Equals(model.Password))
+            {
+                var iden = new GenericIdentity(model.Login);
+
+                //asigna el usuario en el rol
+                var prin = new GenericPrincipal(iden, new[] { "usuario" });
+
+                HttpContext.User = prin;
+
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("err", "Autentitacion incorrecta");
+
+            return View(model);
+
+        }
+    }
+}
---
+++ AutenticacionSimple/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutenticacionSimple.Models
+{
+    public class Usuario
+    {
+        [DisplayName("Nombre de usuario")]
+        public String Login { get; set; }
+
+        [DisplayName("Contraseña")]
+        public String Password { get; set; }
+    }
+}
